ebd05077e67c3bd52c0ed5ddd4d5712a0b60bd5b
Sander Rask Finnemann <sand4384@edu.easj.dk>
2018-09-11 10:48:59 +0200

    Fejl Test

--- primeservice/PrimeService.cs
+++ primeservice/PrimeService.cs
@@ -2,7 +2,11 @@
 
 namespace primeservice
 {
-    public class Class1
+    public class PrimeService
     {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Please create a test first");
+        }
     }
 }
